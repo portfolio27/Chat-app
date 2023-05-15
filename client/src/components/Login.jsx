c4d83a000163edb61bc8b4dc21b07959dedd8ff3
portfolio27 <133584326+portfolio27@users.noreply.github.com>
2023-05-15 10:31:40 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -23,7 +23,7 @@ const Login = (props) => {
 
     const login = async (e) => {
         e.preventDefault();
-        const response = await fetch('/auth/login', {
+        const response = await fetch('https://chat-app-backend-zeta.vercel.app/auth/login', {
             method: "POST",
             headers: {
                 'Content-type': "application/json"
