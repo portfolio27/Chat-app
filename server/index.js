634d25a176d50f0c1aec4b6c4b6a72136ac209e5
portfolio27 <133584326+portfolio27@users.noreply.github.com>
2023-05-15 22:47:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ const server = http.createServer(app);
 const io = require("socket.io")(server, {
     cors: {
       origin: "http://localhost:3000/",
-      methods: ["GET", "POST"],
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
     },
   });
 
