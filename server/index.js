4c1257fb702c0f733bdb447963cd843c3887e3ae
portfolio27 <133584326+portfolio27@users.noreply.github.com>
2023-05-15 10:46:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "https://chat-app-backend-zeta.vercel.app/",
       methods: ["GET", "POST"],
     },
   });
